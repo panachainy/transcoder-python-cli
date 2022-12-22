f3b386f60989a5c85abbef3a0d3a91b063c6c363
Panachai <panachai.ny@gmail.com>
2022-12-22 10:57:44 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -4,15 +4,16 @@ import ffmpeg
 # Create an ArgumentParser object
 parser = argparse.ArgumentParser()
 
-# Add an argument for the input URL
+# Add arguments for the input URL and output file name
 parser.add_argument('input_url', help='URL of the input M3U8 file')
+parser.add_argument('output_file', help='Name of the output MP4 file')
 
 # Parse the command line arguments
 args = parser.parse_args()
 
 # Set the input URL and output file path
 input_url = args.input_url
-output_file = 'results/output.mp4'
+output_file = args.output_file
 
 # Use ffmpeg-python to convert the M3U8 file to an MP4 file
 (
