175db67ee5879311621107ed784d2bdc9957a55b
Panachai <panachai.ny@gmail.com>
2022-12-22 10:57:38 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,7 +1,17 @@
+import argparse
 import ffmpeg
 
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser()
+
+# Add an argument for the input URL
+parser.add_argument('input_url', help='URL of the input M3U8 file')
+
+# Parse the command line arguments
+args = parser.parse_args()
+
 # Set the input URL and output file path
-input_url = 'https://storage.googleapis.com/bucket-name/path-to-file/xxxxxxxxxx.m3u8'
+input_url = args.input_url
 output_file = 'results/output.mp4'
 
 # Use ffmpeg-python to convert the M3U8 file to an MP4 file
