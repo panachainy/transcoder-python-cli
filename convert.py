da14b76064b09a69c7d460919c546152cb821be5
Panachai <panachai.ny@gmail.com>
2022-12-22 13:09:12 +0700

    add doctor command

--- convert.py
+++ convert.py
@@ -11,12 +11,24 @@ load_dotenv()
 parser = argparse.ArgumentParser()
 
 # Add arguments for the input URL, output file name, and upload flag
-parser.add_argument('input_url', help='URL of the input M3U8 file')
+parser.add_argument('input_url', nargs='?', help='URL of the input M3U8 file')
 parser.add_argument('--output_file', default='default_output.mp4', help='Name of the output MP4 file %(prog)s (default: %(default)s)')
 parser.add_argument('--upload', action='store_true', help='Upload the output MP4 file to a Mux asset')
+# Add the `doctor` flag
+parser.add_argument('--doctor', action='store_true', help='Check the current environment for variables required by this code')
 
 # Parse the command line arguments
 args = parser.parse_args()
+
+# Check if the `doctor` flag is set
+if args.doctor:
+    # Check if the required environment variables are set
+    if 'MUX_API_KEY' not in os.environ:
+        print('MUX_API_KEY environment variable is not set')
+    if 'MUX_API_SECRET' not in os.environ:
+        print('MUX_API_SECRET environment variable is not set')
+    print('doctor is done.')
+    raise SystemExit
 
 # Set the input URL and output file path
 input_url = args.input_url
